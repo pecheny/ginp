199dfb9e7c7da464c41b59366962bf100a0267f6
artem pecheny <pecheny@gmail.com>
2024-07-03 14:30:36 +0300

    ButtonToAxis.

---
+++ src/ginp/ButtonToAxis.hx
@@ -0,0 +1,23 @@
+package ginp;
+
+import ginp.api.GameAxis;
+class ButtonToAxis<TB:Axis<TB>, T:Axis<T>> {
+    var buttons:GameButtons<TB>;
+    var pb:TB;
+    var nb:TB;
+
+    public function new(buttons:GameButtons<TB>, pb:TB, nb:TB) {
+        this.buttons = buttons;
+        this.pb = pb;
+        this.nb = nb;
+    }
+
+    public function getDirProjection():Float {
+        var r = 0;
+        if (buttons.pressed(pb))
+            r++;
+        if (buttons.pressed(nb))
+            r--;
+        return r;
+    }
+}

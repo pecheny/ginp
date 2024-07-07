24dc31c5fd139f25ac72abfd747fb26f4b5fa69b
artem pecheny <pecheny@gmail.com>
2024-07-07 05:37:14 +0300

    ButtonMapper: pass all events.

--- src/ginp/ButtonsMapper.hx
+++ src/ginp/ButtonsMapper.hx
@@ -6,7 +6,6 @@ import ginp.api.GameButtonsListener;
 class ButtonsMapper<TIn:Axis<TIn>, TOut:Axis<TOut>> implements GameButtonsListener<TIn>{
     var mapping:Map<TIn,TOut>;
     var target:GameButtonsListener<TOut>;
-    var states:Map<TIn, Bool> = new Map();
 
     public function new(t, ?m) {
         this.mapping = m != null ? m : new Map();
@@ -22,9 +21,6 @@ class ButtonsMapper<TIn:Axis<TIn>, TOut:Axis<TOut>> implements GameButtonsListen
         var bt = mapping[kc];
         if (bt == null)
             return;
-        if (states[kc])
-            return;
-        states[kc] = true;
         target.onButtonDown(bt);
     }
 
@@ -32,13 +28,10 @@ class ButtonsMapper<TIn:Axis<TIn>, TOut:Axis<TOut>> implements GameButtonsListen
         var bt = mapping[kc];
         if (bt == null)
             return;
-        states[kc] = false;
         target.onButtonUp(bt);
     }
 
     public function reset() {
-        for (key in mapping.keys())
-            states[key] = false;
         target.reset();
     }
 }
